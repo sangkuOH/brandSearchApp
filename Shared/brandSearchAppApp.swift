68341af9a4351361aa4456aa236311e8d61eb529
Sangku <sangku824@gmail.com>
2021-10-15 22:15:21 +0900

    Initial Commit

---
+++ Shared/brandSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  brandSearchAppApp.swift
+//  Shared
+//
+//  Created by 오상구 on 2021/10/15.
+//
+
+import SwiftUI
+
+@main
+struct brandSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
